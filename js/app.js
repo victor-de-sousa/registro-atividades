41ac0450629816e11ae535298f7c770bd2a56cdc
victor-de-sousa <victordesousaestudo@gmail.com>
2026-02-20 09:11:04 -0300

    fix: correção do records.push de duração

--- js/app.js
+++ js/app.js
@@ -4,27 +4,26 @@ const recordsList = document.getElementById('records');
 const btnAdd = document.getElementById('btnAdd');
 
 function render() {
-    recordsList.innerHTML = '';
+  recordsList.innerHTML = '';
 
-    records.forEach((r, index) => {
-        const li = document.createElement('li');
-        li.className = 'record';
-        li.textContent = `${r.title} - ${r.duration} min`;
-        recordsList.appendChild(li);
-    });
+  records.forEach((r, index) => {
+    const li = document.createElement('li');
+    li.className = 'record';
+    li.textContent = `${r.title} - ${r.duration} min`;
+    recordsList.appendChild(li);
+  });
 }
 
 btnAdd.addEventListener('click', () => {
-    const title = prompt('Título da atividade: ');
-    if (!title) return;
+  const title = prompt('Título da atividade: ');
+  if (!title) return; 
 
-    const duration = prompt('Duração em minutos (ex: 30):');
+  const duration = prompt('Duração em minutos (ex: 30):');
+  if(!duration) return;
 
-    if(!duration) return;
+  records.push({ title: title.trim(), duration: duration.trim() });
 
-    records.push({title: title.trim(), duration: duration. duration.trim() });
-
-    render();
+  render();
 })
 
 render();
